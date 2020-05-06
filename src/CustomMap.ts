a53249e72a0f6fb659744e5247cc19edf2e75eb6
Goh Sheen An <sheen.an.goh@gmail.com>
2020-05-06 15:36:30 +0800

    Finished Demo

---
+++ src/CustomMap.ts
@@ -0,0 +1,43 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    const mapDiv = document.getElementById(divId);
+    const mapOptions = {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+
+    this.googleMap = new google.maps.Map(mapDiv, mapOptions);
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
